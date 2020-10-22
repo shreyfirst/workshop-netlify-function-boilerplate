5fb13c32c30eef8a27a69f1c1bedfce386d2ce8d
shrey <shreysgupta@gmail.com>
2020-10-21 21:13:36 -0700

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -11,12 +11,24 @@ exports.handler = async (event, context) => {
     }
 });
   
-  transporter.sendMail({
-    from: 'kory18@ethereal.email', 
-    to: "shreysgupta@gmail.com", 
-    subject: "Test email", 
-    text: "Hello world"
-  });
+let message = {
+  from: 'Sender Name <sender@example.com>',
+  to: 'Recipient <shreysgupta@gmail.com>',
+  subject: 'Nodemailer is unicode friendly ✔',
+  text: 'Hello to myself!',
+  html: '<p><b>Hello</b> to myself!</p>'
+};
+
+transporter.sendMail(message, (err, info) => {
+  if (err) {
+      console.log('Error occurred. ' + err.message);
+      return process.exit(1);
+  }
+
+  console.log('Message sent: %s', info.messageId);
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+});
   
   return { statusCode: 200 }
 };
