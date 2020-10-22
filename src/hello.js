5e9bbbfa399035b90f9d5ed06147a9dc2ab24399
shrey <shreysgupta@gmail.com>
2020-10-22 09:02:07 -0700

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -1,19 +1,19 @@
 import nodemailer from "nodemailer"
 
-function queryStringToJSON(queryString) {
-  if(queryString.indexOf('?') > -1){
-    queryString = queryString.split('?')[1];
-  }
-  var pairs = queryString.split('&');
-  var result = {};
-  pairs.forEach(function(pair) {
-    pair = pair.split('=');
-    result[pair[0]] = decodeURIComponent(pair[1] || '');
-  });
-  return result;
-}
-
 exports.handler = async (event, context) => {
+
+  function queryStringToJSON(queryString) {
+    if(queryString.indexOf('?') > -1){
+      queryString = queryString.split('?')[1];
+    }
+    var pairs = queryString.split('&');
+    var result = {};
+    pairs.forEach(function(pair) {
+      pair = pair.split('=');
+      result[pair[0]] = decodeURIComponent(pair[1] || '');
+    });
+    return result;
+  }
 
   const body = queryStringToJSON(event.body);
   const name = body.name;
