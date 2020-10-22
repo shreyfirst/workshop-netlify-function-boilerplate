7b256f024063522e891aba81b61fd9548bf82fd0
shrey <shreysgupta@gmail.com>
2020-10-21 22:32:24 -0700

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,31 +5,29 @@ exports.handler = async (event, context) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
     port: 587,
-    secure: false,
     auth: {
-        user: 'kory18@ethereal.email',
-        pass: 'qjWuKphDgZ1eDrZZbQ'
+        user: 'coby96@ethereal.email',
+        pass: 'wdUJk4WUa7Na8PBb3M'
     }
 });
   
-let message = {
-  from: 'Sender Name <sender@example.com>',
-  to: 'Recipient <shreysgupta@gmail.com>',
-  subject: 'Nodemailer is unicode friendly ✔',
-  text: 'Hello to myself!',
-  html: '<p><b>Hello</b> to myself!</p>'
+let mailOptions = {
+  from: 'coby96@ethereal.email',
+  to: 's@fundsy.io',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
 };
 
-transporter.sendMail(message, (err, info) => {
-  if (err) {
-      console.log('Error occurred. ' + err.message);
-      return process.exit(1);
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
   }
-
-  console.log('Message sent: %s', info.messageId);
-  // Preview only available when sending through an Ethereal account
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 });
-  
-  return { statusCode: 201 }
+
+  return { 
+    statusCode: 200,
+    body: info.response
+  }
 };
