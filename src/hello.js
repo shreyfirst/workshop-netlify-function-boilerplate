9d310131b535b5eb2ff92b2aaa5f6cff4d4a36c0
shrey <shreysgupta@gmail.com>
2020-10-21 23:39:16 -0700

    d

--- src/hello.js
+++ src/hello.js
@@ -1,34 +1,44 @@
-import nodemailer from "nodemailer"
+import sendmail from "sendmail"
 
 exports.handler = async (event, context) => {
 
   let final = "f";
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'coby96@ethereal.email',
-        pass: 'wdUJk4WUa7Na8PBb3M'
-    }
-});
-  
-let mailOptions = {
-  from: 'coby96@ethereal.email',
-  to: 's@fundsy.io',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
+  sendmail({
+    from: 'test@yourdomain.com',
+    to: 's@fundsy.io',
+    replyTo: 'jason@yourdomain.com',
+    subject: 'MailComposer sendmail',
+    html: 'Mail of test sendmail '
+  }, function (err, reply) {
+    console.log(err)
+  })
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-    final = "e" + error
-  } else {
-    console.log('Email sent: ' + info.response);
-    final = "i" + info
-  }
-});
+//   const transporter = nodemailer.createTransport({
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     auth: {
+//         user: 'coby96@ethereal.email',
+//         pass: 'wdUJk4WUa7Na8PBb3M'
+//     }
+// });
+  
+// let mailOptions = {
+//   from: 'coby96@ethereal.email',
+//   to: 's@fundsy.io',
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//     final = "e" + error
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//     final = "i" + info
+//   }
+// });
 
   return { 
     statusCode: 200,
